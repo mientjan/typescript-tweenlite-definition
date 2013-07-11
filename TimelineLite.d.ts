37b971f2f20b190d883394a82e5065f10bb9d933
Mient-jan Stelling <mientjan.stelling@gmail.com>
2013-07-11 15:40:15 +0200

    Update TimelineLite.d.ts

--- TimelineLite.d.ts
+++ TimelineLite.d.ts
@@ -10,4 +10,49 @@ declare class TimelineLite extends TweenLiteStandard
 	public to();
 	public staggerTo();
 	public call(fn: Function);
+	
+	to(target: any, durationSeconds: number, properties: Object, offset?:string):TimelineMax;
+	to(target: any, durationSeconds: number, properties: Object, offset?:number):TimelineMax;
+
+	add(value: any, position?: string, stagger?: number ):TimelineMax;
+	add(value: any, position?: number, stagger?: number ):TimelineMax;
+
+	set(target: any, properties: Object, offset?:string):TimelineMax;
+	set(target: any, properties: Object, offset?:number):TimelineMax;
+	
+	play():TimelineMax;
+	pause():TimelineMax;
+	stop():TimelineMax;
+
+	kill():void;
+	killTweensOf():void;
+
+	totalDuration():number;
+	totalProgress():number;
+	totalTime():number;
+
+	insert(tween:any, timeOrLabel?:number);
+
+	append(tween:any, offset?:string):TimelineMax;
+
+	tweenTo(timeOrLabel:any, vars?:any);
+
+	gotoAndStop(timeOrLabel:any, suppressEvents?:bool);
+	gotoAndPlay(timeOrLabel:any, suppressEvents?:bool);
+
+	reverse(forceResume?:bool);
+
+	addLabel(label:string, time?:number):TimelineMax;
+
+	addCallback(callback: () => void, timeOrLabel:number, params?:any):TimelineMax;
+	addCallback(callback: () => void, timeOrLabel:string, params?:any):TimelineMax;
+
+	timeScale(scale:number):TimelineMax;
+	timeScale():number;
+
+	currentLabel:string;
+	currentProgress:number;
+	repeat:number;
+	repeatDelay:number;
+	yoyo:bool;
 }
